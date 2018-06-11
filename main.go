aa4b6f1f7bce1c544daf53740f2519fbe4543827
nmjmdr <narasimha.gm@gmail.com>
2018-06-12 00:57:50 +1000

    go fmt main

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ const ListenAddress = ":8090"
 
 func main() {
 	router.Start(ListenAddress)
-  fmt.Printf("Server listening on: %s ...",ListenAddress)
-  fmt.Println()
-  // List to quit channel here later, quit gracefully
+	fmt.Printf("Server listening on: %s ...", ListenAddress)
+	fmt.Println()
+	// List to quit channel here later, quit gracefully
 	select {}
 
 }
